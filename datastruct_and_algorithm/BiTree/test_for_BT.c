21ac6648871a7f6f442861d1c603e0f2355bb575
embpgp <522943492@qq.com>
2016-08-11 13:42:53 +0800

    split BT main and .c

---
+++ datastruct_and_algorithm/BiTree/test_for_BT.c
@@ -0,0 +1,32 @@
+/*
+ *File:test_for_BT.c
+ *
+ *Author:rutk1t0r
+ *
+ *Date:2016.8.11
+ *
+ *GPL
+ *
+ */
+
+#include "BT.h"
+#include "SqStack.h"
+
+
+int 
+main(int argc, char *argv[])
+{
+	BiTree root = Create(1, NULL, NULL);
+	InsertLeftForParent(root, 2, root);
+	InsertRightForParent(root, 3, root);
+	InsertLeftForParent(root, 4, root->lchild);
+	InsertRightForParent(root, 5, root->rchild);
+	DeleteRecursionSubtree(&(root->rchild));
+	puts("**********PreOrder***************");
+	PreOrder(root);
+	puts("**********InOrder****************");
+	InOrder(root);
+	puts("**********PostOrder**************");
+	PostOrder(root);
+	return 0;
+}
