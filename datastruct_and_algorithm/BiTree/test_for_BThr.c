34202c88795e6c54fb33d6104b10a3538b117728
embpgp <522943492@qq.com>
2016-09-14 19:38:06 +0800

    learn BiThrTree

--- datastruct_and_algorithm/BiTree/test_for_BThr.c
+++ datastruct_and_algorithm/BiTree/test_for_BThr.c
@@ -7,6 +7,7 @@
  *
  *GPL
  *
+ *method:`gcc -o test_for_BThr BiThrTree.c test_for_BThr.c`
  */
 
  #include "BT.h"
@@ -14,14 +15,15 @@
 
  int main(int argc, char *argv[])
  {
+     /*用最死板的方法创建一个二叉树*/
      BiThrTree root = CreateThr(1, NULL, NULL);
      InsertLeftForParentThr(root, 2, root);
      InsertRightForParentThr(root, 3, root);
      InsertLeftForParentThr(root, 4, root->lchild);
      InsertRightForParentThr(root, 5, root->rchild);
-     BiThrTree v_root = InOrderThr(root);
+     BiThrTree v_root = InOrderThr(root);/*线索化二叉树*/
      printf("中序线索遍历结果如下:\n");
-     VisitThr(v_root);
+     VisitThr(v_root); 
      printf("\n");
      return 0;
  }
